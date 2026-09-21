015c1d1ce72e4abb74d76e4a6ae6e9be1b2ac3bf
Camilo Arango Rodríguez <camilo.bm.x@hotmail.com>
2026-09-21 13:29:14 -0500

    feat: add category model

---
+++ CompraCerca.API/Models/Category.cs
@@ -0,0 +1,12 @@
+namespace CompraCerca.API.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public bool IsActive { get; set; } = true;
+}
